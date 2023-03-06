da6bed3e6776dc3fbf8e1e8469e50ce4aff1855a
renhao <rhwoodpecker@163.com>
2023-03-06 16:46:54 +0800

    keyof comment

--- type-challenges/easy-pick/template.ts
+++ type-challenges/easy-pick/template.ts
@@ -1,3 +1,19 @@
 type MyPick<T, K extends keyof T> = {
     [P in K]: T[P]
 }
+
+// keyof 获取interface、key
+
+interface T1 {
+    a: string
+    b: string
+}
+// 获取keys
+type aKeys = keyof T1
+const k: aKeys = "a"
+
+// 自定义keys
+type bKeys = "a" | "b"
+let k1: bKeys = "a"
+// 可以正常赋值
+k1 = k
